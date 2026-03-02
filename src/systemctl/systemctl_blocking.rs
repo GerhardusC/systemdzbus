2dadf1b99d2319add74df9ed1312306bd66b6ac0
Gerhardus <hardus.cronje@gmail.com>
2026-03-02 18:58:18 +0200

    feat: added reload unit

--- src/systemctl/systemctl_blocking.rs
+++ src/systemctl/systemctl_blocking.rs
@@ -97,6 +97,13 @@ impl<'a> SystemCtlBlocking<'a> {
             .restart_unit(name, &mode.to_string())?)
     }
 
+    /// ReloadUnit(), RestartUnit(), TryRestartUnit(), ReloadOrRestartUnit(), or ReloadOrTryRestartUnit() may be used to restart and/or reload a unit. These methods take similar arguments as StartUnit(). Reloading is done only if the
+    /// unit is already running and fails otherwise. If a service is restarted that isn't running, it will be started unless the "Try" flavor is used in which case a service that isn't running is not affected by the restart. The
+    /// "ReloadOrRestart" flavors attempt a reload if the unit supports it and use a restart otherwise.
+    pub fn reload_unit(&self, name: &str, mode: UnitMode) -> Result<OwnedObjectPath, SystemdError> {
+        Ok(self.get_manager_proxy().reload_unit(name, &mode.to_string())?)
+    }
+
     /// Returns an array of all currently loaded units. Note that units may be known by multiple names at the same name, and hence there might be more unit names loaded than actual units behind them.
     pub fn list_units(&self) -> Result<Vec<Unit>, SystemdError> {
         Ok(self
